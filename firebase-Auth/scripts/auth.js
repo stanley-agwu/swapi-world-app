91db41096d0973b1e53ac66ad90dd7bede894045
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2021-01-13 16:46:46 +0100

    Added auth state check

--- firebase-Auth/scripts/auth.js
+++ firebase-Auth/scripts/auth.js
@@ -1,3 +1,12 @@
+// listen for auth status changes
+auth.onAuthStateChanged(user => {
+  if (user) {
+    console.log('user logged in: ', user);
+  } else {
+    console.log('user logged out');
+  }
+})
+
 // signup
 const signupForm = document.querySelector('#signup-form');
 signupForm.addEventListener('submit', (e) => {
@@ -8,14 +17,11 @@ signupForm.addEventListener('submit', (e) => {
   const password = signupForm['signup-password'].value;
 
   // sign up the user
-  auth.createUserWithEmailAndPassword(email, password).then(cred => {
-    console.log(cred.user);
-
+  auth.createUserWithEmailAndPassword(email, password)
     // close the signup modal & reset form
     const modal = document.querySelector('#modal-signup');
     M.Modal.getInstance(modal).close();
     signupForm.reset();
-  });
 });
 
 // logout
@@ -37,12 +43,10 @@ loginForm.addEventListener('submit', (e) => {
   const password = loginForm['login-password'].value;
 
   // log the user in
-  auth.signInWithEmailAndPassword(email, password).then((cred) => {
-    console.log(cred.user);
+  auth.signInWithEmailAndPassword(email, password)
     // close the signup modal & reset form
     const modal = document.querySelector('#modal-login');
     M.Modal.getInstance(modal).close();
     loginForm.reset();
-  });
 });
 
