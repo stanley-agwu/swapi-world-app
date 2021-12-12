45da5efeb794f1c8f03870e23fc1084293e004da
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2021-12-12 12:49:29 +0100

    Updated planetReducer and actionTypes

--- planets_app/src/reducers/PlanetReducer.ts
+++ planets_app/src/reducers/PlanetReducer.ts
@@ -1,13 +1,32 @@
-interface DefaultStateI {
+import {PLANET_FAILED, PLANET_LOADING, PLANET_SUCCESS, PlanetInfoType} from "../actions/PlanetActionTypes";
 
+interface DefaultStateI {
+    loading: boolean,
+    planet?: PlanetInfoType
 }
 
 const defaultState: DefaultStateI = {
-
+    loading: false
 };
 
 const planetReducer = (state: DefaultStateI = defaultState, action: any): DefaultStateI => {
-    return state;
+    switch (action.type) {
+        case PLANET_FAILED:
+            return {
+                loading: false
+            }
+        case PLANET_LOADING:
+            return {
+                loading: true
+            }
+        case PLANET_SUCCESS:
+            return {
+                loading: false,
+                planet: action.payload
+            }
+        default:
+            return state;
+    }
 };
 
 export default planetReducer;
