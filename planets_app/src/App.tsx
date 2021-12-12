65d93cc6479cb41d0a888773915c07651702c585
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2021-12-12 13:31:28 +0100

    Connected action creators to FE

--- planets_app/src/App.tsx
+++ planets_app/src/App.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, {FC, useState} from 'react';
 import './App.css';
+import {useDispatch, useSelector} from "react-redux";
+import {RootStore} from "./store/Store";
+import {getPlanets} from "./actions/PlanetActions";
 
-const App = () => {
+const App: FC = () => {
+  const planetState = useSelector((state: RootStore) => state.planet)
+  const [planets, setPlanets] = useState<string>('')
+  const dispatch = useDispatch()
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => (
+      setPlanets(e.target.value)
+  )
+  const handleSubmit = (e:  React.MouseEvent<HTMLButtonElement>) => dispatch(getPlanets(planets))
+
+  console.log('planets are: ', planetState)
+
   return (
     <div className="App">
+      <input type='text' onChange={handleChange} />
+      <button onClick={handleSubmit}>Search</button>
     </div>
   );
 }
