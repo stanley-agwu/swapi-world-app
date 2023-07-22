feda02c2df615b383500d1857eccfb528ac15b0c
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2023-07-22 21:22:29 +0200

    feat(app): jest config

---
+++ swapi-world-app/jest.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  preset: 'ts-jest',
+  bail: 1,
+  verbose: true,
+  moduleFileExtensions: ['js', 'ts', 'tsx'],
+  transform: {},
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleDirectories: ['node_modules', 'test', 'src'],
+  collectCoverage: true,
+  coverageReporters: ['text'],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!<rootDir>/node_modules/',
+    '!<rootDir>/node_modules/',
+    '!<rootDir>/src/reportWebVitals.ts',
+    '!<rootDir>/src/**/useDisneyCharactersData.ts',
+    '!<rootDir>/src/index.tsx',
+  ],
+  coverageReporters: ['text'],
+  coveragePathIgnorePatterns: [
+    'node_modules',
+    'index.tsx',
+    '<rootDir>/src/index.tsx',
+    '<rootDir>/src/reportWebVitals.ts',
+    '<rootDir>/src/hooks/useDisneyCharactersData.ts',
+  ],
+  coverageDirectory: '<rootDir>/coverage/',
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+};
