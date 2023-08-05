277d0849da09299ca5a41621fad5137ea805f5af
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2023-08-05 12:22:50 +0200

    feat(app): url config: updated endpoints and url

--- swapi-world-app/src/common/core/config.ts
+++ swapi-world-app/src/common/core/config.ts
@@ -1,27 +1,37 @@
 interface CoreConfig {
   endpoints: {
-    root: string;
-    planets: string;
     people: string;
-    spaceShip: string;
+    planets: string;
+    films: string;
+    species: string;
+    vehicles: string;
+    starships: string;
   };
   routes: {
-    planets: string;
     people: string;
-    spaceShip: string;
+    planets: string;
+    films: string;
+    species: string;
+    vehicles: string;
+    starships: string;
   };
 }
 
 export const coreConfig: CoreConfig = {
   endpoints: {
-    root: '',
-    planets: '',
-    people: '',
-    spaceShip: '',
+    people: 'https://swapi.dev/api/people/',
+    planets: 'https://swapi.dev/api/planets/',
+    films: 'https://swapi.dev/api/films/',
+    species: 'https://swapi.dev/api/species/',
+    vehicles: 'https://swapi.dev/api/vehicles/',
+    starships: 'https://swapi.dev/api/starships/',
   },
   routes: {
-    planets: '',
     people: '',
-    spaceShip: '',
+    planets: '',
+    films: '',
+    species: '',
+    vehicles: '',
+    starships: '',
   },
 };
