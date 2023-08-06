3754545ccfef226cf49a2071174ba3761453035b
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2023-08-06 23:57:06 +0200

    feat(app): imported react icons and updated queries with interfaces

--- swapi-world-app/src/common/api/services/swapi.ts
+++ swapi-world-app/src/common/api/services/swapi.ts
@@ -1,30 +1,30 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 import { coreConfig } from 'common/core/config';
-import type { People } from 'common/models';
+import type { IFilms, IPeople, IPlanets, ISpecies, IStarships, IVehicles } from 'common/models';
 
 // Define a service using a base URL and expected endpoints
 export const swapiApi = createApi({
   reducerPath: 'swapiApi',
   baseQuery: fetchBaseQuery({ baseUrl: coreConfig.endpoints.root }),
   endpoints: (builder) => ({
-    getPeople: builder.query<People, string>({
-      query: (pageNumber) => coreConfig.endpoints.people.format(pageNumber),
+    getPeople: builder.query<IPeople, string>({
+      query: (pageNumber = '1') => coreConfig.endpoints.people.format(pageNumber),
     }),
-    getPlanets: builder.query<People, string>({
-      query: (pageNumber) => coreConfig.endpoints.people.format(pageNumber),
+    getPlanets: builder.query<IPlanets, string>({
+      query: (pageNumber = '1') => coreConfig.endpoints.planets.format(pageNumber),
     }),
-    getFilms: builder.query<People, string>({
-      query: (pageNumber) => coreConfig.endpoints.people.format(pageNumber),
+    getFilms: builder.query<IFilms, string>({
+      query: (pageNumber = '1') => coreConfig.endpoints.films.format(pageNumber),
     }),
-    getSpecies: builder.query<People, string>({
-      query: (pageNumber) => coreConfig.endpoints.people.format(pageNumber),
+    getSpecies: builder.query<ISpecies, string>({
+      query: (pageNumber = '1') => coreConfig.endpoints.species.format(pageNumber),
     }),
-    getVehicles: builder.query<People, string>({
-      query: (pageNumber) => coreConfig.endpoints.people.format(pageNumber),
+    getVehicles: builder.query<IVehicles, string>({
+      query: (pageNumber = '1') => coreConfig.endpoints.vehicles.format(pageNumber),
     }),
-    getStarships: builder.query<People, string>({
-      query: (pageNumber) => coreConfig.endpoints.people.format(pageNumber),
+    getStarships: builder.query<IStarships, string>({
+      query: (pageNumber = '1') => coreConfig.endpoints.starships.format(pageNumber),
     }),
   }),
 });
