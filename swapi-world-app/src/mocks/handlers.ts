abbf2f0ab7c48323cbed2e583df787c58651f6a6
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2023-09-08 16:46:30 +0200

    feat(test): msw handlers and dashboard mocks

--- swapi-world-app/src/mocks/handlers.ts
+++ swapi-world-app/src/mocks/handlers.ts
@@ -1,6 +1,15 @@
 import { rest } from 'msw';
 
+import { coreConfig } from 'common/core/config';
+
+import peopleMocks from './results/peopleMocks';
+import planetsMocks from './results/planetsMocks';
+import starshipsMocks from './results/starshipsMocks';
+
+const baseEndPoint = coreConfig.endpoints.root;
+
 export const handlers = [
-  // Handles a GET /user request
-  rest.get('/swapi', (_, res, ctx) => res(ctx.json({}))),
+  rest.get(`${baseEndPoint}planets/`, (_, res, ctx) => res(ctx.json(planetsMocks))),
+  rest.get(`${baseEndPoint}people/`, (_, res, ctx) => res(ctx.json(peopleMocks))),
+  rest.get(`${baseEndPoint}starships/`, (_, res, ctx) => res(ctx.json(starshipsMocks))),
 ];
