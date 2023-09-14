c6c2c6e4a9f942878124ab39ea5e10c94137f8cc
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2023-09-14 17:39:17 +0200

    feat(app): reduced number of columns to enhance responsiveness, updated styles

--- swapi-world-app/src/common/utils/categoriesTitlesKeys.ts
+++ swapi-world-app/src/common/utils/categoriesTitlesKeys.ts
@@ -5,9 +5,9 @@ interface CategoriesTitlesKeys {
 export const categoriesTitlesKeys: CategoriesTitlesKeys = {
   planetsKeys: ['name', 'terrain', 'population', 'climate'],
   filmsKeys: ['title', 'director', 'producer', 'release_date'],
-  residentKeys: ['name', 'birth_year', 'gender', 'skin_color', 'hair_color'],
-  pilotsKeys: ['name', 'birth_year', 'gender', 'skin_color', 'hair_color'],
-  speciesKeys: ['name', 'classification', 'designation', 'average_height', 'language'],
+  residentKeys: ['name', 'birth_year', 'gender', 'skin_color'],
+  pilotsKeys: ['name', 'birth_year', 'gender', 'skin_color'],
+  speciesKeys: ['name', 'classification', 'designation', 'language'],
   starshipsKeys: ['name', 'manufacturer', 'crew', 'passengers'],
-  vehiclesKeys: ['name', 'model', 'manufacturer', 'crew', 'passengers', 'consumables'],
+  vehiclesKeys: ['name', 'model', 'manufacturer', 'passengers'],
 };
